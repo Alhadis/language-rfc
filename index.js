b7999984cc53fb67e545797a4290e81c92dbeede
Alhadis <gardnerjohng@gmail.com>
2025-08-13 18:16:38 +1000

    Initialise repository with project's current state
    These files were originally staged for inclusion in `language-etc`, with
    a last-minute re-evaluation deeming the changeset too excessive for that
    project's usual standards.

---
+++ index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+module.exports = {
+	disposables: null,
+
+	/**
+	 * Activate package and handle event subscriptions.
+	 * @api private
+	 */
+	activate(){
+		this.disposables = new (require("atom").CompositeDisposable)(
+			this.observeEditors(this.watchRFCs.bind(this)),
+		);
+	},
+
+	/**
+	 * Deactivate package.
+	 * @api private
+	 */
+	deactivate(){
+		this.disposables && this.disposables.dispose();
+		this.disposables = null;
+	},
+
+	/**
+	 * Invoke a callback for every {@link TextEditor} open in the workspace.
+	 * @param {Function} callback
+	 * @return {Disposable}
+	 * @api private
+	 */
+	observeEditors(callback){
+		if(atom.packages.initialPackagesActivated)
+			return atom.workspace.observeTextEditors(callback);
+		const disposable = atom.packages.onDidActivateInitialPackages(() => {
+			disposable.dispose();
+			this.disposables.delete(disposable);
+			setTimeout(() => atom.textEditors.editors.forEach(callback), 100);
+		});
+		return disposable;
+	},
+
+	/**
+	 * Auto-detect IETF RFC documents when opened.
+	 * @param {TextEditor} editor
+	 * @return {void}
+	 * @api private
+	 */
+	watchRFCs(editor){
+		const name = editor.getFileName();
+		if(!name || atom.textEditors.getGrammarOverride(editor)) return;
+		switch(editor.getGrammar().scopeName){
+			case "text.plain":
+			case "text.plain.null-grammar":
+				if(/^(?:rfc|bcp|fyi|ien|std)\d+\.txt$/.test(name))
+					atom.textEditors.setGrammarOverride(editor, "text.rfc");
+		}
+	},
+};
